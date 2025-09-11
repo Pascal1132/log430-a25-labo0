c297119f95daf25329c0b7f7b6c95271a6884a71
Pascal1132 <p.parent.1@outlook.com>
2025-09-11 20:36:15 +0000

    tests

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -12,3 +12,19 @@ def test_app():
 
 def test_addition():
     assert my_calculator.addition(2, 4) == 6
+
+def test_subtraction():
+    assert my_calculator.subtraction(10, 3) == 7
+    assert my_calculator.last_result == 7
+
+def test_multiplication():
+    assert my_calculator.multiplication(3, 5) == 15
+    assert my_calculator.last_result == 15
+
+def test_division():
+    assert my_calculator.division(10, 2) == 5
+    assert my_calculator.last_result == 5
+
+def test_division_by_zero():
+    assert my_calculator.division(5, 0) == "Erreur : division par zéro"
+    assert my_calculator.last_result == "Error"
